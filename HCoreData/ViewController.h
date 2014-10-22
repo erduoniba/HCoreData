4ead44f49b317d0cffb8682bfc42848c7b1fa8be
Harry <328418417@qq.com>
2014-10-22 11:19:45 +0800

    Initial Commit

---
+++ HCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCoreData
+//
+//  Created by Harry on 14/10/22.
+//  Copyright (c) 2014年 Harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HCoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HCoreData
+//
+//  Created by Harry on 14/10/22.
+//  Copyright (c) 2014年 Harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
